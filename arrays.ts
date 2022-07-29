b4249f44ae04b6a6bb0fb0b43ee7b9020581c271
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-29 18:46:23 +0200

    Typed Arrays

--- arrays.ts
+++ arrays.ts
@@ -4,3 +4,17 @@ const dates = [new Date(), new Date()]
 const carsByMarke: string[][] = [
     ['f150', 'corolla', 'skyline']
 ]
+
+// help with inference when extracting values
+const car = carMakers[0]
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100)
+
+// Help with 'MAP' and so on
+
+carMakers.map((car:string): string => {
+    return car.toLocaleUpperCase()
+})
+
