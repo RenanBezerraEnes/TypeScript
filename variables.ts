c0d760917c88459b8014ae850d63e25a6e1d4e07
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-29 14:41:33 +0200

    Annotations with Variables

---
+++ variables.ts
@@ -0,0 +1,11 @@
+let apples: number = 5
+let speed: string = "10km"
+let hashName: boolean = true;
+
+let nothingMuch: null = null
+let nothing: undefined = undefined
+
+
+
+// built in objects
+let now: Date = new Date()
