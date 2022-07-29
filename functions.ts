d3b0602ed7924b64a8b60f9ffbd83a614264cc07
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-29 16:47:13 +0200

    It is important to specify the return type

--- functions.ts
+++ functions.ts
@@ -1,3 +1,9 @@
 const add = (a:number, b:number): number => {
     return a + b
 }   
+
+
+// It is important to specify the return type otherwise I can face a problem 
+// const subtract = (a:number, b:number) => {
+//     a + b
+// }   
