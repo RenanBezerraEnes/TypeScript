ca404b7577cb7873de936d61120b4bf63a2d5350
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-29 14:52:25 +0200

    Object Literal

--- variables.ts
+++ variables.ts
@@ -9,3 +9,20 @@ let nothing: undefined = undefined
 
 // built in objects
 let now: Date = new Date()
+
+// Array
+let colors: string[] = ['red', 'green', 'blue']
+let myNumbers: number[] = [1,2,3]
+let truths: boolean[] = [true,true,false]
+
+// Class
+class Car {
+
+}
+let car: Car = new Car()
+
+// Object Literal
+let point: {x: number, y:number} = {
+    x: 10,
+    y:20
+}
