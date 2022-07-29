5f80c8aa74c68d3cee555231ee557a0fb3bbdeca
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-29 16:54:36 +0200

    Void means this function won't return nothing

--- functions.ts
+++ functions.ts
@@ -15,3 +15,7 @@ function divide(a: number, b: number): number {
 const multiply = function(a:number, b: number): number {
     return a * b
 }
+
+const logger = (message: string): void => {
+    console.log(message)
+}
