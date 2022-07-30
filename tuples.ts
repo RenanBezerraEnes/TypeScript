a002240e752ebfe42bc418b24d04029102bb7d88
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-30 16:19:58 +0200

    Another example with Tuples and why

--- tuples.ts
+++ tuples.ts
@@ -11,3 +11,14 @@ type Drink = [string, boolean, number]
 const pepsi: Drink = ['brown', true, 40]
 const sprite: Drink = ['clear', true, 50]
 const tea: Drink = ['red', false, 70]
+
+
+
+// Why Tuples
+const carSpecs: [number,number] = [400, 3354] 
+
+
+const carStats = {
+    horsepower: 400,
+    weight: 3354
+}
