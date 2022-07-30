7210b17f5f86b96e42a7a0be4a8b547a612f3ff0
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-30 17:23:35 +0200

    Fields in Classes, constructor

--- classes.ts
+++ classes.ts
@@ -1,8 +1,16 @@
 class Vehicle{
+    // Special function for arguments -> line 11
+    constructor(public color: string) {
+    }
+
     protected honk(): void {
         console.log("VOID VOID")
     }
 }
+
+const vehicle = new Vehicle('orange')
+console.log(vehicle.color);
+
 
 class Car extends Vehicle {
     private drive(): void {
