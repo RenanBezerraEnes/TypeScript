6ff3d51de5b7e805d04e7ddf8e76211297ab85b2
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-30 16:31:25 +0200

    Applying interface

--- Interfaces.ts
+++ Interfaces.ts
@@ -1,10 +1,17 @@
+interface Vehicle {
+    name: string,
+    year: number,
+    broken: boolean
+}
+
+// OBS: This could be a state for example...
 const oldCivic = {
     name: 'Civic',
     year: 2000,
     broken: true
 }
 
-const printVehicle = (vehicle: {name: string, year: number, broken: boolean}): void => {
+const printVehicle = (vehicle: Vehicle): void => {
     console.log('Name: ${vehicle.name}');
     console.log('Year: ${vehicle.year}');
     console.log('Broken: ${vehicle.broken}');
