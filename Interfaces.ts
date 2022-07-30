24dae93408345ba2eec2fb1e894460f3a4e80202
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-30 16:26:54 +0200

    working with const + function, but without interfaces yet

---
+++ Interfaces.ts
@@ -0,0 +1,13 @@
+const oldCivic = {
+    name: 'Civic',
+    year: 2000,
+    broken: true
+}
+
+const printVehicle = (vehicle: {name: string, year: number, broken: boolean}): void => {
+    console.log('Name: ${vehicle.name}');
+    console.log('Year: ${vehicle.year}');
+    console.log('Broken: ${vehicle.broken}');
+}
+
+printVehicle(oldCivic)
