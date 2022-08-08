59dca7eb6a9dbf3874d79bcbdcccb2f3f94ba7de
Renan Bezerra <renanbezerraenes@icloud.com>
2022-08-08 21:25:20 +0200

    Generic Constraints

--- generics.ts
+++ generics.ts
@@ -49,3 +49,30 @@ function printAnything<T>(arr: T[]):void {
 
 printAnything<string>(['a', 'b', 'c'])
 printAnything<number>([2,4,5])
+
+// Generic Constraints
+
+class Car {
+    print(){
+        console.log('I am the car')
+    }
+}
+
+class House {
+    print() {
+        console.log('I am the house');
+    }
+}
+
+interface Printable {
+    print(): void
+}
+
+function printHousesOrHouses<T extends Printable>(arr: T[]):void {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].print()       
+    }
+}
+
+printHousesOrHouses<House>([new House(), new House()])
+printHousesOrHouses<Car>([new Car(), new Car()])
