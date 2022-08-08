ee6b888c73021f07eed0f64fabdf1176c8b139d0
Renan Bezerra <renanbezerraenes@icloud.com>
2022-08-08 21:16:21 +0200

    Generic Function using <T>

--- generics.ts
+++ generics.ts
@@ -23,3 +23,26 @@ class ArrayOfGenerics<T> {
 }
 
 new ArrayOfGenerics<string>(['a', 'b', 'c'])
+
+// Examples generics with functions
+
+// This functions printStrings and printNumbers are not generic and, it's the same code twice, not a good approach
+function printStrings(arr: string[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])   
+    }
+}
+
+function printNumbers(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])   
+    }
+}
+
+// Making a generic function and reusable
+
+function printAnything<T>(arr: T[]):void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])   
+    }
+}
