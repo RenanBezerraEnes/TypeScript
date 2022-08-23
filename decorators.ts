5b03ad35b51253d13db63539835ddc967486898c
Renan Bezerra <renanbezerraenes@icloud.com>
2022-08-23 17:55:46 +0200

    Decorator to get a Error

---
+++ decorators.ts
@@ -0,0 +1,27 @@
+class Boat {
+    color: string = 'red'
+
+    get formattedColor(): string {
+        return `This boats color is ${this.color}`
+    }
+
+    @logError
+    pilot(): void {
+        throw new Error()
+        console.log('swish');
+    }
+}
+
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+   const method = desc.value;
+
+   desc.value = function () {
+    try {
+        method()
+    } catch (e) {
+        console.log(`Opsss error in the boat`)
+    }
+   }
+}
+
+new Boat().pilot()
