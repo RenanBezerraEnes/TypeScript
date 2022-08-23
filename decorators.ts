8a72aaeb34150b6fc95e23d1ec565be774284f55
Renan Bezerra <renanbezerraenes@icloud.com>
2022-08-23 17:58:03 +0200

    Decorator used as factories

--- decorators.ts
+++ decorators.ts
@@ -5,23 +5,25 @@ class Boat {
         return `This boats color is ${this.color}`
     }
 
-    @logError
+    @logError('ops the boat was sunk')
     pilot(): void {
         throw new Error()
         console.log('swish');
     }
 }
-
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-   const method = desc.value;
-
-   desc.value = function () {
-    try {
-        method()
-    } catch (e) {
-        console.log(`Opsss error in the boat`)
-    }
-   }
+function logError(errorMessage: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+        const method = desc.value;
+     
+        desc.value = function () {
+         try {
+             method()
+         } catch (e) {
+             console.log(errorMessage)
+         }
+        }
+     }
 }
+
 
 new Boat().pilot()
