c9bfad5794bab4ad9ac5fc2c3e25e5cb820a5581
Renan Bezerra <renanbezerraenes@icloud.com>
2022-08-23 20:54:36 +0200

    Tie a amount of information with a method and decorator

--- metadata.ts
+++ metadata.ts
@@ -1,18 +1,34 @@
 import 'reflect-metadata'
 
-const plane = {
-    color: 'red'
-};
+// const plane = {
+//     color: 'red'
+// };
 
-Reflect.defineMetadata('note', 'hi there', plane, 'color')
+// Reflect.defineMetadata('note', 'hi there', plane, 'color')
 
-const relatedToColor = Reflect.getMetadata('note', plane, 'color')
-console.log(relatedToColor);
+// const relatedToColor = Reflect.getMetadata('note', plane, 'color')
+// console.log(relatedToColor);
 
-Reflect.defineMetadata('note', 'hi there', plane)
-Reflect.defineMetadata('height', 10, plane)
+// Reflect.defineMetadata('note', 'hi there', plane)
+// Reflect.defineMetadata('height', 10, plane)
 
-const note = Reflect.getMetadata('note', plane)
-const height = Reflect.getMetadata('height', plane)
+// const note = Reflect.getMetadata('note', plane)
+// const height = Reflect.getMetadata('height', plane)
 
-console.log(note , height)
+// console.log(note , height)
+
+class Plane {
+    color:string = 'red'
+
+    @markFunction
+    fly():void {
+        console.log('FLYING')
+    }
+}
+
+function markFunction(target: Plane, key: string) {
+    Reflect.defineMetadata('secret', 123, target, key)
+}
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly')
+console.log(secret);
